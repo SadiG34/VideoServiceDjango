93d625f18dc3a0f2b8a5d02b3cebdd46c4a30ace
SadiG34 <super.sadig@list.ru>
2024-05-14 22:49:52 +0300

    new styles

--- webvideo/videoservice/forms.py
+++ webvideo/videoservice/forms.py
@@ -5,6 +5,8 @@ class CommentForm(forms.Form):
     comment = forms.CharField(widget=forms.Textarea(
         attrs={
             'class': 'form-control',
-            'placeholder': 'Оставьте комментарий!'
+            'placeholder': 'Оставьте комментарий!',
+            'rows': 3,
+            'style': 'background: radial-gradient(circle at 24.1% 68.8%,rgb(10, 10, 10) 0%,  rgb(50, 50, 50) 99.4%); color: grey;'
         }
     ))
